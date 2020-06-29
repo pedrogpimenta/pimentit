d37e509e573f01a918984f954b208af9ab8cebc3
Pedro Pimenta <pedrogpimenta@users.noreply.github.com>
2020-06-29 10:13:02 +0200

    Fix typo

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
       colors: {
         primary: 'var(--color-primary)',
         background: 'var(--color-background)',
-        backgroundSoft: 'var(--color-background--soft)'
+        backgroundSoft: 'var(--color-background--soft)',
         foreground: 'var(--color-foreground)',
         foregroundSoft: 'var(--color-foreground--soft)',
         
